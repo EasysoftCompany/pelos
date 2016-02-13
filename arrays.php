ed90554282e55985922d6ea4473f14676e18d814
Gerardo Rico <g_rico_c@hotmail.com>
2016-02-13 01:07:40 -0600

    Cleaning cache

--- arrays.php
+++
@@ -1,5 +0,0 @@
-<?php
-#Aqui se incluyen todos los precios y descripciones para la pagina
-$price = [100,200,300,400];
-$desc = ['Manta Bien chida','Manta Kawaii','Manta Medio Kawaii','Manta Ultra Kawaii'];
-
